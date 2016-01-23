71c93a0a1b826b661ed980a3b285a1ef2b790328
Simon Støvring <simonstoevring@gmail.com>
2016-01-23 18:10:00 +0100

    Initial commit

---
+++ dldr-multiple/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+"""
+Download programs from DR TV.
+"""
+
+__title__ = 'dldr-multiple'
+__version__ = '1.0.0'
+__author__ = 'Simon Støvring'
+__license__ = 'MIT License'
+
+from .dldr-multiple import run
---
+++ dldr-multiple/dldr-multiple.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""dldr-multiple
+Download programs from dr.dk/tv by supplying multiple links.
+
+Usage:
+  dldr-multiple.py --input=FILE [--slug-name] [--output=DIR]
+  dldr-multiple.py (-h | --help)
+  dldr-multiple.py --version
+
+Options:
+  -h --help                              Show this screen.
+  --version                              Show version.
+  --slug-name                            Use slug as file name.  E.g. "huset-pa-christianshavn-1" instead of "Huset På Christianshavn (1)"
+  -i FILE, --input FILE                  File containing links to programs that should be downloaded. Links must be separated by a new line.
+  -o DIR, --output=DIR                   Output directory for the downloaded files.
+"""
+from docopt import docopt
+from schema import Schema, Use, And, Or
+import os
+import subprocess
+import re
+
+def create_command(url, slug_name = False, output_dir = None):
+  cmd = "dldr"
+  cmd = "{0} --slug-name".format(cmd) if slug_name else cmd
+  cmd = "{0} --output \"{1}\"".format(cmd, output_dir) if output_dir else cmd
+  cmd = "{0} {1}".format(cmd, url)
+  return cmd
+
+def start_download_for_urls(urls, slug_name = False, output_dir = None):
+  for url in urls:
+    subprocess.call(create_command(url, slug_name, output_dir), shell=True)
+
+def valid_urls(file_path):
+  lines = open(file_path).read().splitlines()
+  regex = re.compile(r"https?:\/\/(www\.)?dr.dk\/tv\/")
+  return [ l for l in lines if regex.search(l) ]
+  
+def run():
+  args = docopt(__doc__, version="dldr-multiple 1.0.0")
+  s = Schema({
+    '--help': bool,
+    '--version': bool,
+    '--slug-name': bool,
+    '--input': Or(os.path.exists, None, error='Input file does not exist.'),
+    '--output': Or(os.path.exists, None, error='Output directory does not exist.'),
+  })  
+  args = s.validate(args)  
+  urls = valid_urls(args['--input'])
+  start_download_for_urls(urls, args['--slug-name'], args['--output'])
+
+if __name__ == "__main__":
+  run()
